4af438ee3f21ad5f1074d266065e5f22f643259f
teodorstupnicki <teodor.stupnicki@gmail.com>
2022-07-13 00:42:40 +0200

    Project structure update

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem passing arguments: {err}");
+        eprintln!("Problem passing arguments: {err}");
         process::exit(1);
     });
     
     if let Err(e) = gru::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
