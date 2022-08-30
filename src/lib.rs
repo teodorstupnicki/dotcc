8f18ba87290063d1b11b4cd18567172d24dc6c2f
teodorstupnicki <teodor.stupnicki@gmail.com>
2022-08-30 12:25:53 +0200

    Updated Config::new to use an iterator

--- src/lib.rs
+++ src/lib.rs
@@ -20,13 +20,17 @@ pub struct Config {
 }
 
 impl Config {
-     pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments")
-        }
+     pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string.")
+        };
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name.")
+        };
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
         Ok(Config { query, filename, case_sensitive })
     }
--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,7 @@ use std::{env, process, error::Error};
 /// Search for a pattern in a file and display the lines that contain it.
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem passing arguments: {err}");
         process::exit(1);
     });
