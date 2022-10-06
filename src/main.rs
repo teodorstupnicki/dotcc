e229e9fd4cf5766b10d6c4e8fe6674deeac0fbff
teodorstupnicki <teodor.stupnicki@gmail.com>
2022-10-06 23:48:33 +0200

    Added some classes

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,15 @@ pub struct Repository {
     pub path: String
 }
 
+pub struct State {
+    pub command: String,
+    pub argument: String
+}
+
+pub trait Command {
+    
+}
+
 fn main() {
     let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem passing arguments: {err}");
